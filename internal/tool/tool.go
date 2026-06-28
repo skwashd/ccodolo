eea5208b9d8d347edd3c012e8e50a133861f6619
Dave Hall <skwashd@gmail.com>
2026-06-28 10:00:33 -0400

    Bump gh from 2.89.0 to 2.95.0

--- internal/tool/tool.go
+++ internal/tool/tool.go
@@ -472,7 +472,7 @@ var builtinCatalog = []Tool{
 		Name:        "gh",
 		Category:    "utils",
 		Description: "GitHub CLI",
-		DefaultTag:  "2.89.0",
+		DefaultTag:  "2.95.0",
 		Instructions: []string{
 			`RUN curl -fsSL "https://github.com/cli/cli/releases/download/v{{.Tag}}/gh_{{.Tag}}_linux_$(dpkg --print-architecture).tar.gz" \` + "\n" +
 				`  | tar xz --strip-components=2 -C /usr/local/bin --wildcards '*/bin/gh'`,
